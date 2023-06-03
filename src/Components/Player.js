2bd638c3f965098efc0f5777c7b7883d3cef7992
Phat07 <nguyenhoangphat852@gmail.com>
2023-06-03 10:25:47 +0700

    Ex_10

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,58 +1,31 @@
-import React, {Component} from "react";
-
-export default class Player extends Component{
-    render(){
+import React from "react";
+import { Players } from "../Shared/ListOfPlayers";
+import { useState } from "react";
+export default function Player(){
+    const [player,setPlayer] = useState([])
         return(
             <div className="container">
-                <div className="column">
+                {Players.map((player)=>(
+                    <div className="column">
                     <div className="card">
-                        <img src="assets/cr.jpg"/>
-                        <h3>Cristino Ronaldo</h3>
-                        <p className="title">Manchester United</p>
-                        <p><button>Detail</button></p>
+                        <img src={player.img}/>
+                        <h3>{player.name}</h3>
+                        <p className="title">{player.club}</p>
+                        <p><button onClick={()=>{setPlayer(player)}}><a href="#popup1" id="openPopUp">Detail</a></button></p>
                     </div>
                 </div>
-                <div className="column">
-                    <div className="card">
-                        <img src="assets/kante.jpg"/>
-                        <h3>Kante</h3>
-                        <p className="title">Chelsea</p>
-                        <p><button>Detail</button></p>
+                ))}
+                <div id="popup1" className="overlay">
+                    <div className="popup">
+                        <img src={player.img} />
+                        <h2>{player.name}</h2>
+                        <a className="close" href="#">&times;</a>
+                        <div className="content">
+                            {player.info}
+                        </div>
                     </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <img src="assets/messi.jpg"/>
-                        <h3>Messi</h3>
-                        <p className="title">PSG</p>
-                        <p><button>Detail</button></p>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <img src="assets/neymar.jpg"/>
-                        <h3>Neymar</h3>
-                        <p className="title">PSG</p>
-                        <p><button>Detail</button></p>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <img src="assets/kane.jpg"/>
-                        <h3>Kane</h3>
-                        <p className="title">Tottemham</p>
-                        <p><button>Detail</button></p>
-                    </div>
-                </div>
-                <div className="column">
-                    <div className="card">
-                        <img src="assets/haaland.jpg"/>
-                        <h3>Haaland</h3>
-                        <p className="title">Manchester City</p>
-                        <p><button>Detail</button></p>
-                    </div>
+
                 </div>
             </div>
         )
-    }
 }
