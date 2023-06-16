1b8c0f52ec950a5288af74f4b0ed3d679ffebec2
Phat07 <nguyenhoangphat852@gmail.com>
2023-06-16 22:59:28 +0700

    Ex_17

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
+import Contact from './components/Contact';
 import './App.css';
-import Navigation from './Components/Navigation';
-import Player from './Components/Player';
-import Footer from './Components/Footer';
+
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <Player/>
-      <Footer/>
+     <Contact/>
     </div>
   );
 }
